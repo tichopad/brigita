aec8d82c8762d9b16073090ac2bffc5d9825ce67
Michael Tichopad <michaeltichopad01@gmail.com>
2021-02-18 20:34:36 +0100

    Auto-detect CSV delimiter

--- src/handleCsv.js
+++ src/handleCsv.js
@@ -1,25 +1,66 @@
 import * as csv from '@fast-csv/parse';
-import { filter, groupBy, indexBy, map, omit, pathEq, pipe, prop, reject, values } from 'ramda';
+import {
+  always,
+  filter,
+  groupBy,
+  ifElse,
+  indexBy,
+  indexOf,
+  lt,
+  map,
+  nth,
+  omit,
+  pathEq,
+  pipe,
+  prop,
+  reduce,
+  reject,
+  values,
+  __,
+} from 'ramda';
 
+/**
+ * @param {number} range
+ * @param {number} limit
+ */
 const findTargets = (range, limit) => {
   return pipe(
     groupBy(prop('Sample')),
     map(indexBy(prop('Fluor'))),
     reject(pathEq(['FAM', 'Cq'], 'NaN')),
     omit(['NC', 'PC']),
-    reject((row) => parseFloat(row.FAM.Cq) >= limit || parseFloat(row.HEX.Cq) >= limit),
-    filter((row) => Math.abs(parseFloat(row.FAM.Cq) - parseFloat(row.HEX.Cq)) >= range),
+    reject(({ FAM, HEX }) => parseFloat(FAM.Cq) >= limit || parseFloat(HEX.Cq) >= limit),
+    filter(({ FAM, HEX }) => Math.abs(parseFloat(FAM.Cq) - parseFloat(HEX.Cq)) >= range),
     values
   );
 };
 
+/** @param {string} csvString */
+const detectDelimiter = (csvString) => {
+  const separators = [',', ';', '|', '\t'];
+  const isNegative = lt(__, 0);
+  const characterAtIndex = nth(__, csvString);
+  const indexOfCharacter = indexOf(__, csvString);
+
+  return pipe(
+    map(indexOfCharacter),
+    reduce((previous, current) => {
+      return previous === -1 || (current !== -1 && current < previous) ? current : previous;
+    }, -1),
+    ifElse(isNegative, always(','), characterAtIndex)
+  )(separators);
+};
+
+/** @param {string} csvString */
 const parseCsv = (csvString) => {
   let rows = [];
 
+  const csvStringSnippet = csvString.slice(0, 100);
+  const delimiter = detectDelimiter(csvStringSnippet);
   const parseStream = csv.parseString(csvString, {
-    delimiter: ';',
+    delimiter,
     discardUnmappedColumns: true,
-    headers: true
+    headers: true,
   });
 
   return new Promise((resolve, reject) => {
